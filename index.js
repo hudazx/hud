38d1d4f29ec7ba31cb6e7453efd96e564e2076ff
huda <92105551+hudazx@users.noreply.github.com>
2022-01-13 17:37:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("hello");
+	bot.chat("halo bang")
+	bot.chat("kalau chat ini spam, ignore aja ya :)")
+	bot.chat("instagram.com/hud0s")
+	bot.chat("Nyoba Afk 1 Hari")
+	;
 });
 
 bot.on('time', function(time) {
